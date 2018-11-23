ab61d594eac93c057efe15f7c68ee522b683f423
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-11-23 12:07:38 +0100

    zeer kleine update

--- kraanopdracht/src/be/kul/gantry/domain/Problem.java
+++ kraanopdracht/src/be/kul/gantry/domain/Problem.java
@@ -321,8 +321,8 @@ public class Problem {
         ArrayList<Move> solution = new ArrayList<>();
 
         Slot inputslot = new Slot(-1,-1,-1,-1,-1,-1,-1,-1,Slot.SlotType.STORAGE,null),outputslot = new Slot(-1,-1,-1,-1,-1,-1,-1,-1,Slot.SlotType.STORAGE,null);
-        Gantry inputGangtry=gantries.get(0);
-        Gantry outputGangtry=gantries.get(1);
+        Gantry inputGantry=gantries.get(0);
+        Gantry outputGantry=gantries.get(1);
         
 
         Map<Integer,SlotTree> rows = new HashMap<>();
